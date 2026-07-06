9ff3e67dc4eb0cd8a4ff77df7ab6dcef31f6fe8a
Zeynep Kizilkaya <zeynepyildiz.kizilkaya@gmail.com>
2026-07-06 15:20:52 +0300

    changed the numbers to make it more sharpened

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ else:
     blur = cv2.GaussianBlur(gray, (5, 5), 0)
 
     # detect edges using canny
-    edges = cv2.Canny(blur, 50, 150)
+    edges = cv2.Canny(blur, 75, 200)
 
     # show edge image
     cv2.imshow("Canny Edge Detection", edges)
